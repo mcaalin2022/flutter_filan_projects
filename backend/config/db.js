40efd5d1e30bc8a0b62d5545e2fbf8add2226d5b
qaasimmuuse86-ctrl <qaasimmuuse86@gmail.com>
2026-02-07 19:07:49 +0300

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,12 +4,12 @@ const env = require('./env');
 
 const connectDB = async () => {
     try {
-        const conn = await mongoose.connect(env.MONGODB_URI);
+        
+module.exports = connectDB;
+const conn = await mongoose.connect(env.MONGODB_URI);
         console.log(`MongoDB Connected: ${conn.connection.host}`);
     } catch (error) {
         console.error(`Error: ${error.message}`);
         process.exit(1);
     }
 };
-
-module.exports = connectDB;
